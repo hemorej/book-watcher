22e3a006219bc66f7daa035529006012ac13c3a3
j_a_ <jerome.arfouche@gmail.com>
2016-12-21 10:58:57 -0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/foundation.css">
         <link href="/css/app.css" rel="stylesheet">
+	<title>{{ config('app.name', 'Laravel') }}</title>
         <style>
         	.red{
         		color: red;
