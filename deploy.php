f07fb1dab4b5898ddf188b955ab665892ed10d23
j_a_ <jerome.arfouche@gmail.com>
2020-08-02 16:08:45 -0400

    bugfix: composer step broken in deployer

--- deploy.php
+++ deploy.php
@@ -1,4 +1,5 @@
 <?php
+// replace host, deploy_user with values
 namespace Deployer;
 
 require 'recipe/laravel.php';
@@ -12,13 +13,13 @@ set('repository', 'git@github.com:hemorej/book-watcher.git');
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
 
-set('deploy_path', '');
+set('deploy_path', '/home/deploy_user/host/steidl');
 
 // Shared files/dirs between deploys 
 set('writable_mode', 'chmod');
 set('writable_chmod_mode', 777);
 set('writable_chmod_recursive', true);
-add('shared_files', []);
+add('shared_files', ['steidl.sqlite']);
 add('shared_dirs', []);
 
 // Writable dirs by web server 
@@ -33,21 +34,29 @@ set('allow_anonymous_stats', false);
 
 // Hosts
 
-host('')
+host('host')
     ->set('deploy_path', get('deploy_path'))
-	->user('')
+	->user('deploy_user')
     ->set('branch', 'master');
     
-// Tasks
-
-task('build', function () {
-    run('cd {{release_path}} && build');
+task('deploy:vendor', function(){
+    run('cd {{release_path}} && php /home/deploy_user/.php/composer/composer install --no-interaction --no-suggest --optimize-autoloader');
 });
+
+// Tasks
+task('deploy', [
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendor',
+    'deploy:failed',
+    'deploy:unlock'
+]);
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
-
 before('deploy:symlink', 'artisan:migrate');
-
