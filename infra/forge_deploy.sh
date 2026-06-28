242cff96037c761a9aaa0dbc7414847168704dbb
j_a_ <jerome.arfouche@pm.me>
2026-06-27 20:06:07 -0400

    update infra

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -9,7 +9,7 @@ npm run build
 $FORGE_PHP artisan optimize
 $FORGE_PHP artisan storage:link
 $FORGE_PHP artisan migrate --force
-mkdir public
+
 ln -s /mnt/volume-tor1-01/imprintfonts public/fonts
 
 $ACTIVATE_RELEASE()
