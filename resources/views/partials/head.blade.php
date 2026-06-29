79a803243e41f69ea1e5e9d9d7b987699344db63
j_a_ <jerome.arfouche@pm.me>
2026-06-29 11:36:41 -0400

    icon/theme fix for home screen

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,6 +6,11 @@
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="manifest" href="/site.webmanifest">
+<meta name="theme-color" content="#FBFAF7">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
