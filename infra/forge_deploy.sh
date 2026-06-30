01a49a7df9ba0abb6895e3c0f25f52102f4ecf81
j_a_ <jerome.arfouche@pm.me>
2026-06-30 15:46:33 -0400

    small prod deploy perf optimizations

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -10,6 +10,10 @@ $FORGE_PHP artisan optimize
 $FORGE_PHP artisan storage:link
 $FORGE_PHP artisan migrate --force
 
+$FORGE_PHP artisan config:cache
+$FORGE_PHP artisan route:cache
+$FORGE_PHP artisan view:cache
+
 ln -s /mnt/volume-tor1-01/imprintfonts public/fonts
 
 $ACTIVATE_RELEASE()
