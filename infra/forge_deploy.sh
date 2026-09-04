dd450ea6d08e6bc4b35e46c7b05fc08fc4f13dfa
j_a_ <jerome.arfouche@pm.me>
2026-09-04 15:01:19 -0400

    restart php-fpm on deploy

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -19,3 +19,8 @@ ln -s /mnt/$VOLUME_NAME/imprintfonts public/fonts
 $ACTIVATE_RELEASE()
 
 $RESTART_QUEUES()
+
+if [ -f /etc/php/$FORGE_PHP_VERSION/fpm/php-fpm.conf ]; then
+    ( flock -w 10 9 || exit 1
+        echo 'Restarting FPM...'; sudo -S service $FORGE_PHP_FPM reload ) 9>/tmp/fpmlock
+fi
